5327f95102f29ae53a67d5e29ccc01e8da7b27e4
emmprokak <89413115+emmprokak@users.noreply.github.com>
2022-09-18 18:22:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/*
+MNS Food Management
+Copyright (C) 2022 Prokakis Emmanouil
+MNS Food Management is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+MNS Food Management is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with MNS Food Management. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 const express = require('express');
 const app = express();
 const dotenv = require("dotenv");
